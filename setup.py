54afd7ceb1695449d53757867c9d3b63993c8751
Ding-Yang Liu <199928ltyos@gmail.com>
2024-11-30 02:33:26 +0800

    rename to pymaftools

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyMAF',
+    name='pymaftools',
     version='0.1',
+    author = "xu62u4u6",
+    author_email="199928ltyos@gmail.com",
     packages=find_packages(),
     install_requires=[
-        'pandas', 'numpy', 'matplotlib', 'seaborn', 
+        'pandas', 
+        'numpy', 
+        'matplotlib', 
+        'seaborn', 
     ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    url='https://github.com/xu62u4u6/pymaftools',
+    
 )
