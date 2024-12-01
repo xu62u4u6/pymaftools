2c9aacd485d5955c31be8460ecc22c3adda0c4c2
Ding-Yang Liu <199928ltyos@gmail.com>
2024-12-01 15:41:56 +0800

    update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.1',
     author = "xu62u4u6",
     author_email="199928ltyos@gmail.com",
+    description='pymaftools is a Python package for handling and analyzing Mutation Annotation Format (MAF) files. It provides utilities for data manipulation and visualization, including classes for MAF parsing and oncoplot generation.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'pandas', 
@@ -19,5 +22,4 @@ setup(
     ],
     license='MIT',
     url='https://github.com/xu62u4u6/pymaftools',
-    
 )
