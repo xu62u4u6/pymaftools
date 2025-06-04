4436f77d068b3147e1e9c03021c6b291fded24c2
Ding-Yang Liu <199928ltyos@gmail.com>
2025-06-05 00:52:59 +0800

    refactor: rename to mutation_enrichment_test, add fisher option, adjust minimum_mutations

--- pymaftools/core/PivotTable.py
+++ pymaftools/core/PivotTable.py
@@ -361,34 +361,53 @@ class PivotTable(pd.DataFrame):
         binary_pivot_table[:] = (binary_pivot_table != False) # avoid class transform
         return binary_pivot_table
 
-    def chisquare_test(self, group_col, group1, group2, alpha=0.05, minimum_mutations=2):
+    def mutation_enrichment_test(
+        self,
+        group_col,
+        group1,
+        group2,
+        alpha=0.05,
+        minimum_mutations=2,
+        method="chi2"  # or "fisher"
+        ):
+
         binary_pivot_table = self.to_binary_table()
         sample_metadata = binary_pivot_table.sample_metadata
+
         subset1 = binary_pivot_table.subset(samples=sample_metadata[group_col] == group1)
         subset2 = binary_pivot_table.subset(samples=sample_metadata[group_col] == group2)
 
         df = pd.DataFrame(index=binary_pivot_table.index, 
                         columns=[f"{group1}_True", f"{group1}_False", f"{group2}_True", f"{group2}_False"])
-        
+
         df[f"{group1}_True"] = subset1.sum(axis=1)
-        df[f"{group1}_False"] = len(subset1.columns) - df[f"{group1}_True"] 
+        df[f"{group1}_False"] = len(subset1.columns) - df[f"{group1}_True"]
 
         df[f"{group2}_True"] = subset2.sum(axis=1)
         df[f"{group2}_False"] = len(subset2.columns) - df[f"{group2}_True"]
 
-        df = df[(df[f"{group1}_True"] + df[f"{group2}_True"]) > minimum_mutations]
+        # Filter out mutations not observed enough
+        df = df[(df[f"{group1}_True"] >= minimum_mutations) | (df[f"{group2}_True"] >= minimum_mutations) ]
 
-        def get_chisquare_p_value(row):
-            contingency_table = row.values.reshape(2, 2)
-            _, p, _, _ = chi2_contingency(contingency_table)
+        def get_p_value(row):
+            contingency_table = row.values.astype(int).reshape(2, 2)
+            if method == "chi2":
+                _, p, _, _ = chi2_contingency(contingency_table)
+            elif method == "fisher":
+                _, p = fisher_exact(contingency_table)
+            else:
+                raise ValueError(f"Unsupported method: {method}")
             return p
 
-        df["p_value"] = df.apply(get_chisquare_p_value, axis=1)
+        df["p_value"] = df.apply(get_p_value, axis=1)
+
         p_values = df["p_value"].values
         reject, adjusted_p_value, _, _ = multipletests(p_values, method="fdr_bh", alpha=alpha)
 
         df["adjusted_p_value"] = adjusted_p_value
         df["is_significant"] = reject
+        df["test_method"] = method
+
         return df
     
     def merge(tables: list["PivotTable"], fill_table_na_with=False, fill_metadata_na_with=np.nan, join="outer"):
